3d678bef7e34863f71f7a0855b2b30d357cbb6ad
Vincent <Vincent.ahlin99@gmail.com>
2024-06-12 14:52:32 +0200

    add another cookie

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -48,6 +48,14 @@ exports.login = async (req, res) => {
   const token = generateToken(user);
 
   req.session.token = token;
+
+  res.cookie("access_token", token, {
+    maxAge: 90 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
+
   console.log("Token set in session:", req.session.token);
 
   return res.status(200).json({ user: noPassword });
