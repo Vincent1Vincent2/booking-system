e65eed5c11acf74e4629d206dd18c5601ba616e6
Vincent <Vincent.ahlin99@gmail.com>
2024-06-12 14:38:51 +0200

    Removing rate limiter for now, error in g run

--- server/src/app.js
+++ server/src/app.js
@@ -3,21 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const cookieSession = require("cookie-session");
 const cors = require("cors");
-const rateLimit = require("express-rate-limit");
 const authRoutes = require("./routes/authRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const roomRoutes = require("./routes/roomRoutes");
 
 const app = express();
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
-
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:9000" }));
-app.use(limiter);
 app.use(
   cookieSession({
     name: "token",
