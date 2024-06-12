1ab9fcb3f16d2b25158d586e3549c9e26c02401e
Vincent <Vincent.ahlin99@gmail.com>
2024-06-12 12:05:19 +0200

    remove old code

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -5,9 +5,9 @@ const authenticateToken = (req, res, next) => {
 
   console.log(token);
 
-  if (!token && !backup) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401);
 
-  jwt.verify(token || backup, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
