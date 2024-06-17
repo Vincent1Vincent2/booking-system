00d1f884cf5ba03539fcf0f0ae1d38957f805eff
Vincent <Vincent.ahlin99@gmail.com>
2024-06-17 11:11:03 +0200

    Tests for removing and editing bookings made

--- client/cypress/e2e/my-bookings.cy.ts
+++ client/cypress/e2e/my-bookings.cy.ts
@@ -1,4 +1,4 @@
-describe("Check bookings", () => {
+describe("My bookings", () => {
   beforeEach(() => {
     cy.clearBookings();
 
@@ -15,5 +15,29 @@ describe("Check bookings", () => {
 
   it("check my bookings", () => {
     cy.get("[data-cy=bookingsContainer]").children().should("have.length", 3);
+  });
+
+  it("remove booking", () => {
+    cy.get("[data-cy=deleteBooking]").first().click();
+    cy.get("[data-cy=confirmDeleteBtn]").click();
+
+    cy.get("[data-cy=bookingsContainer]").children().should("have.length", 2);
+    cy.get("[data-cy=myBookingsBadge]").contains(2);
+  });
+
+  it("edit booking", () => {
+    cy.get("[data-cy=editBooking]").first().click();
+
+    const today = new Date();
+    const newDate = new Date(today);
+    newDate.setDate(today.getDate() + 1);
+
+    cy.get("[data-cy=dateSelector]").clear().type(newDate.toISOString());
+    cy.get("[data-cy=saveBookingBtn]").click();
+
+    cy.get("[data-cy=bookingsContainer]")
+      .children()
+      .first()
+      .should("contain", newDate);
   });
 });
