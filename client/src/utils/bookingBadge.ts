c02cf69b0febc870aaa6592711484366f5c4883f
Vincent <Vincent.ahlin99@gmail.com>
2024-06-18 10:05:46 +0200

    Add a number badge to indicate how many active bookings a user has

---
+++ client/src/utils/bookingBadge.ts
@@ -0,0 +1,7 @@
+const { getMyBookings } = require("../utils/api/bookings");
+
+export async function updateBookingBadge() {
+  const bookingsBadge = document.getElementById("bookingsBadge");
+  const bookings = await getMyBookings();
+  bookingsBadge!.innerHTML = bookings.length || "";
+}
